5a7ce981760b2d0edbfda2855d97e2c444f3eaeb
ruchita <ruchitasagalgile04@gmail.com>
2024-02-17 11:47:03 +0530

    call apply bind explain

---
+++ Basic of javascript/output base questions/callapplybind.js
@@ -0,0 +1,50 @@
+//explain call
+
+var sample = {
+  name: "ruchi s",
+  hello: function (thing) {
+    console.log(this.name + " says hello " + thing);
+  },
+};
+
+var data = {
+  name: "riya",
+};
+sample.hello.call(data, "world");
+
+//takes the context for particular function and argment which we
+//pass inside the function
+
+//explain apply
+
+var person = {
+  name: "priya",
+  hello: function (thing) {
+    console.log(this.name + " says hello " + thing);
+  },
+};
+
+var othername = {
+  name: "tina",
+};
+
+person.hello.apply(othername, ["world"]);
+
+//explain bind :
+//only take context and it will return a complete new function with
+//context
+
+var dummyData = {
+  name: "aditya",
+  hello: function (thing) {
+    console.log(this.name + " says hello " + thing);
+  },
+};
+
+var namste = {
+  name: "priyanka",
+};
+
+let newBindData = dummyData.hello.bind(namste);
+
+newBindData("world");
