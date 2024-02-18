6be27804653895f0c2085656770187c6d95a8b27
ruchita <ruchitasagalgile04@gmail.com>
2024-02-18 05:50:11 +0530

    compose and flat

--- Basic of javascript/ployfill for compose method/app.js
+++ Basic of javascript/ployfill for compose method/app.js
@@ -17,10 +17,35 @@ function multiplyFour(a) {
 
 const compose = (...functions) => {
   return (args) => {
-    return functions.reduceRight((args, fn) => fn(args), args);
+    return functions.reduceRight((arr, fn) => fn(arr), args);
   };
 };
 
 const evalute = compose(addFive, substractTwo, multiplyFour);
 
 console.log(evalute(5));
+
+//flat function
+
+let arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6, [7, 8], 9],
+  [10, 11, 12],
+];
+
+function customFlat(arr, depth = 1) {
+  let result = [];
+  arr.forEach((arr) => {
+    if (Array.isArray(arr) && depth > 0) {
+      result.push(...customFlat(arr, depth - 1));
+    } else {
+      result.push(arr);
+    }
+  });
+  return result;
+}
+
+
+
+console.log(customFlat(arr, 5));
